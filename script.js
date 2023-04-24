5d626754382d1826220ab7f512743f8c6d86b24b
Durgesh <101012986+Nikhilsingh2003@users.noreply.github.com>
2023-04-24 07:21:20 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,22 @@
+
+const navLinks = document.querySelectorAll('nav a');
+
+navLinks.forEach((link) => {
+  link.addEventListener('click', (event) => {
+    event.preventDefault();
+    const section = document.querySelector(link.getAttribute('href'));
+    section.scrollIntoView({ behavior: 'smooth' });
+  });
+});
+
+
+const mobileMenuBtn = document.querySelector('.mobile-menu-btn');
+const mobileNav = document.querySelector('.mobile-nav');
+
+mobileMenuBtn.addEventListener('click', () => {
+  mobileNav.classList.toggle('show');
+});
+
+const currentYear = new Date().getFullYear();
+const footerYear = document.querySelector('footer p');
+footerYear.innerHTML += ` ${currentYear}`;
